509b78c408ec66086e013395504d4b3b5b7a019c
Ago1702 <85801687+Ago1702@users.noreply.github.com>
2025-08-02 18:04:50 +0200

    User generator

--- src/slurm_load/user.py
+++ src/slurm_load/user.py
@@ -1,3 +1,5 @@
+import numpy.random as rnd
+
 class User(object):
     '''
     Class User: a simple class for keeping user's info
@@ -14,3 +16,47 @@ class User(object):
     
     def __str__(self):
         return f"User {self.usr}:{self.usr_id} Group {self.group}:{self.group_id}"
+    
+    def __lt__(self, other):
+        if isinstance(other, User):
+            return self.usr_id < other.usr_id
+        raise NotImplementedError('Not Implemented Comparison') 
+
+    
+class UserGenerator(object):
+    '''
+    Class UserGenerator: A simple generator for users
+    '''
+
+    def __init__(self, min_usr:int, max_usr:int, min_group:int, max_group:int):
+        if min_usr > max_usr:
+            raise ValueError("min_usr cannot be greater than max_usr")
+        if min_group > max_group:
+            raise ValueError("min_group cannot be greater than max_group")
+        self.min_usr = min_usr
+        self.max_usr = max_usr
+        self.min_group = min_group
+        self.max_group = max_group 
+        
+    def set_seed(self, seed:int):
+        rnd.seed(seed)
+
+    def generate_users(self) -> tuple[set[User], int]:
+        n_group = rnd.randint(self.min_group, self.max_group)
+        users = set([User()])
+        n_account = rnd.randint(self.min_usr, self.max_usr)
+        for i in range(1, n_account):
+            gr_i = rnd.randint(1, n_group)
+            usr = f'user{i}'
+            usr_id = 1000 + i
+            group = f'group{gr_i}'
+            group_id = 1000 + gr_i
+            users.add(User(usr, usr_id, group, group_id))
+        return users, n_group
+        
+
+
+
+    
+
+    
--- src/slurm_load/utils.py
+++ src/slurm_load/utils.py
@@ -4,13 +4,16 @@ def read_users_sim(p='tests/users.sim') -> list[User]:
     user_list = []
     with open(p, mode='r') as f:
         while line := f.readline():
+            line = line[:-1]
             data = line.split(':')
             if len(data) != 4:
                 return []
+            data[1] = int(data[1])
+            data[3] = int(data[3])
             user_list.append(User(*data))
     return user_list
 
 def print_users_sim(p, users:list[User]):
     with open(p, mode='w') as f:
-        lines = [repr(user) for user in users]
+        lines = [repr(user) + '\n' for user in users]
         f.writelines(lines)
