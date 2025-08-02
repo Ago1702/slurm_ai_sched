981b8661063283534ee66ab1e935f5a421462acf
Ago1702 <85801687+Ago1702@users.noreply.github.com>
2025-08-02 03:34:13 +0200

    added a print user function

--- src/slurm_load/utils.py
+++ src/slurm_load/utils.py
@@ -1,6 +1,6 @@
 from slurm_load.user import User
 
-def read_users(p='tests/users.sim') -> list[User]:
+def read_users_sim(p='tests/users.sim') -> list[User]:
     user_list = []
     with open(p, mode='r') as f:
         while line := f.readline():
@@ -9,3 +9,8 @@ def read_users(p='tests/users.sim') -> list[User]:
                 return []
             user_list.append(User(*data))
     return user_list
+
+def print_users_sim(p, users:list[User]):
+    with open(p, mode='w') as f:
+        lines = [repr(user) for user in users]
+        f.writelines(lines)
