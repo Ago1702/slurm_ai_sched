57145645d9e37aeb062d70ca1bd71bf46232e870
Ago1702 <85801687+Ago1702@users.noreply.github.com>
2025-08-05 02:11:28 +0200

    Node random generator

--- src/slurm_topo/node.py
+++ src/slurm_topo/node.py
@@ -1,4 +1,6 @@
 from abc import ABC, abstractmethod
+from enum import Enum
+import numpy.random as rnd
 
 class Node(object):
     '''
@@ -45,6 +47,56 @@ class Node(object):
         return slurm_format
     
 
+class NodeGenerator(object):
+    '''
+    Class NodeGenerator: Random Computing Node generator
+    '''
+    def __init__(self, min_proc:int=0, max_proc:int=7, min_mem:int=6, max_mem:int=33, min_sock:int=1, max_sock:int=3, min_gres:int=1, max_gres:int=9):
+        self.min_proc = min_proc
+        self.max_proc = max_proc
+        self.min_mem = min_mem
+        self.max_mem = max_mem
+        self.min_sock = min_sock
+        self.max_sock = max_sock
+        self.min_gres = min_gres
+        self.max_gres = max_gres
+
+        self.many_min_proc = max_proc * 2
+        self.many_max_proc = max_proc * 8
+        self.big_min_mem = max_mem * 4
+        self.big_max_mem = max_mem * 16
+
+
+    def set_seed(self, seed:int):
+        rnd.seed(seed)
+
+    def generate_node(self, name:str, num:int|tuple[int], features:list=[], big_mem:bool=False, gpu:bool=False, many_cores:bool=False):
+        if many_cores:
+            procs = rnd.randint(self.many_min_proc, self.many_max_proc)
+            features.append('ManyCores')
+        else:
+            procs = rnd.randint(self.min_proc, self.max_proc)
+        procs = 1 if procs == 0 else procs * 4
+        sockets = rnd.randint(self.min_sock, self.max_sock)
+        sockets = procs if sockets > procs else sockets
+        if big_mem:
+            memory = rnd.randint(self.big_min_mem, self.big_max_mem)
+            features.append('BigMem')
+        else:
+            memory = rnd.randint(self.min_mem, self.max_mem)
+        memory *= 4000
+        if not gpu:
+            return Node(name, procs, sockets, num, memory, features=features)
+        gres = rnd.randint(self.min_gres, self.max_gres)
+        return Node(name, procs, sockets, num, memory, gres=gres, features=features)
+
+class TopologyGenerator(object):
+    '''
+    Class TopologyGenerator: A simple topology generator
+    '''
+
+    def __init__(self):
+        pass
 
 if __name__ == '__main__':
         default = Node('DEFAULT', 12, 2, memory=48000)
@@ -53,5 +105,16 @@ if __name__ == '__main__':
         g = Node('g', 12, 2, 1, gres=2, features=['IB','CPU-G'])
         mem = Node('b', 12, 2, 1, memory=512000, gres=2, features=['IB','CPU-G', 'BigMem'])
         nodelist = [default, nodes_n, nodes_m, g, mem]
+        gen = NodeGenerator()
+        node_a = gen.generate_node('a', 4, ['IB', 'CPU-A'])
+        nodelist.append(node_a)
+        node_s = gen.generate_node('s', 8, ['IB', 'CPU-S'], big_mem=True)
+        nodelist.append(node_s)
+        node_k = gen.generate_node('k', 4, ['IB', 'CPU-K'], gpu=True)
+        nodelist.append(node_k)
+        node_mc = gen.generate_node('f', 4, ['IB', 'CPU-F'], many_cores=True)
+        nodelist.append(node_mc)
+        node_gen = gen.generate_node('i', 2, ['IB', 'CPU-I'], big_mem=True, gpu=True, many_cores=True)
+        nodelist.append(node_gen)
         for node in nodelist:
             print(node.slurm_formatting())
